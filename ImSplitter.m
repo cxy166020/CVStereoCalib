bbc399269a9667c8641a665b3ac0b87f98a693c7
cxy166020 <cxy166020@gmail.com>
2011-08-11 16:03:02 -0700

    Add resize option

--- ImSplitter.m
+++ ImSplitter.m
@@ -1,3 +1,10 @@
+function ImSplitter(CalibWidth, CalibHeight)
+
+if nargin < 2
+    CalibWidth = 0;
+    CalibHeight = 0;
+end
+
 im = dir('*.jpg');
 
 NumOfIm = size(im, 1);
@@ -7,10 +14,18 @@ for i=1:NumOfIm
     
     pic = imread(im(i).name);
     
-    [height width channel] = size(pic);
+    [height, width, chanel] = size(pic); %#ok<NASGU>
     
-    left  = imresize(pic(:, 1:width/2, :), [height/2 width/2]);
-    right = imresize(pic(:, width/2+1:end, :), [height/2 width/2]);
+    if CalibWidth~=0 && CalibHeight~=0
+        CalibWidth = CalibWidth/2;
+        CalibHeight = CalibHeight/2;
+    else
+        CalibWidth = width/2;
+        CalibHeight = height/2;
+    end
+    
+    left  = imresize(pic(:, 1:width/2, :), [CalibHeight CalibWidth]);
+    right = imresize(pic(:, width/2+1:end, :), [CalibHeight CalibWidth]);
     
     imwrite(left,  ['l_' im(i).name]);
     imwrite(right, ['r_' im(i).name]);
