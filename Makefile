e0df65a3e5d383b6f0796292d2754433be99732e
cxy166020 <cxy166020@gmail.com>
2011-08-11 12:14:33 -0700

    toGPU compiles

--- Makefile
+++ Makefile
@@ -2,36 +2,47 @@ CC = g++
 CFLAGS = -Wall -c -O2
 LDFLAGS = -lopencv_highgui -lopencv_core -lopencv_calib3d
 
-
 PROG = stereoCalib
 UDIS = undistort
-SCAL = singleCalib
-HDRS = stereovision.h CommandlineUtils.h
-SRCS = main.cpp stereovision.cpp CommandlineUtils.cpp
+TGPU = toGPU
+
+CSTV = stereovision.h stereovision.cpp
+CMMD = CommandlineUtils.h CommandlineUtils.cpp
+
+MAIN = main.cpp
+MGPU = ToGPU.cpp
+MUDI = undistort.cpp
 
 OBJS = main.o stereovision.o CommandlineUtils.o
 UOBJ = undistort.o stereovision.o CommandlineUtils.o
+TOBJ = ToGPU.o stereovision.o CommandlineUtils.o
 
-PROJECT : $(PROG) $(UDIS)
+PROJECT : $(PROG) $(UDIS) $(TGPU)
+
+$(PROG) : $(OBJS) 
+	$(CC) $(LDFLAGS) $^ -o $@
 
 $(UDIS) : $(UOBJ)
 	$(CC) $(LDFLAGS) $^ -o $@
 
-$(PROG) : $(OBJS)
+$(TGPU) : $(TOBJ)
 	$(CC) $(LDFLAGS) $^ -o $@
 
 undistort.o : undistort.cpp
 	$(CC) $(CFLAGS) $^
 
 main.o : main.cpp
-	$(CC) $(CFLAGS) $^ 
+	$(CC) $(CFLAGS) $^
 
 stereovision.o : stereovision.h stereovision.cpp
-	$(CC) $(CFLAGS) $^ 
+	$(CC) $(CFLAGS) $^
 
 CommandlineUtils.o : CommandlineUtils.h CommandlineUtils.cpp
 	$(CC) $(CFLAGS) $^
 
+ToGPU.o : ToGPU.cpp
+	$(CC) $(CFLAGS) $^
+
 
 clean:
-	rm *.o $(PROG) $(UDIS)
+	rm *.o $(PROG) $(UDIS) $(TGPU)
--- ToGPU.cpp
+++ ToGPU.cpp
@@ -3,10 +3,30 @@
 #include <fstream>
 
 #include <opencv/cv.h>
+#include <opencv/cxmisc.h>
+#include <opencv/cvaux.h>
+#include <opencv/highgui.h>
 
 #include "CommandlineUtils.h"
 #include "stereovision.h"
 
+void GetUndistortedImageName(const std::string& OriginalImName, 
+			     std::string& UndistortedImName,
+			     const std::string& OutputPath);
+
+void InitConfig(std::ofstream& ofm, 
+		std::string ConfigName,
+		int ImSetSize);
+
+void FillConfig(std::ofstream& ofm, 
+		std::string UndistortedImNameL, 
+		std::string UndistortedImNameR, 
+		CvMat* Intrinsic_L,
+		CvMat* Intrinsic_R,
+		CvMat* rotation,
+		CvMat* translation);
+
+void CloseConfig(std::ofstream& ofm);
 
 int main(int argc, char** argv)
 {
@@ -35,14 +55,14 @@ int main(int argc, char** argv)
   int CalibSize = 0;
   char** pCalibFile = ArgList.GetArgsByOption("-c", CalibSize);
 
-  std::string pCalibFile = pCalibFile[0];
+  std::string CalibFile = pCalibFile[0];
 
 
   // Get output config file name
   int ConfigSize = 0;
-  char** pConfig = ArgList.GetArgsByOption("-g", ConfigSize);
+  char** pConfigName = ArgList.GetArgsByOption("-g", ConfigSize);
 
-  std::string config = pConfig[0];
+  std::string ConfigName = pConfigName[0];
 
 
   if(ImNameL == NULL)
@@ -66,16 +86,68 @@ int main(int argc, char** argv)
 
   StereoVision sv;
 
-  sv.calibrationLoad(CalibFile[0]);
+  sv.calibrationLoad(CalibFile.c_str());
+
+  std::ofstream ofm;
+
+  InitConfig(ofm, ConfigName, ImSetSizeL);
+
+  // --------------------------------------------
+  CvMat* Intrinsic_L = cvCreateMat(3,3,CV_32FC1);
+  CvMat* Intrinsic_R = cvCreateMat(3,3,CV_32FC1);
+  CvMat* rotation    = cvCreateMat(3,3,CV_32FC1);
+  CvMat* translation = cvCreateMat(3,1,CV_32FC1);
 
   // Undistort and save images
   for(int i=0; i<ImSetSizeL; i++)
     {
-      
+      std::string UndistortedImNameL; 
+      std::string UndistortedImNameR;
+
+      GetUndistortedImageName(std::string(ImNameL[i]),
+			      UndistortedImNameL,
+			      OutputPath);
+
+      GetUndistortedImageName(std::string(ImNameR[i]),
+			      UndistortedImNameR,
+			      OutputPath);
+
+      sv.undistortImage(ImNameL[i], ImNameR[i], 
+			UndistortedImNameL.c_str(), 
+			UndistortedImNameR.c_str(),
+			Intrinsic_L, Intrinsic_R,
+			rotation, translation);
+
+      FillConfig(ofm, UndistortedImNameL, UndistortedImNameR, 
+		 Intrinsic_L, Intrinsic_R, rotation, translation);
     }
 
+  CloseConfig(ofm);
+
+
+  cvReleaseMat(&Intrinsic_L);
+  cvReleaseMat(&Intrinsic_R);
+  cvReleaseMat(&rotation);
+  cvReleaseMat(&translation);
   
   return 0;
+}
+
+void GetUndistortedImageName(const std::string& OriginalImName, 
+			     std::string& UndistortedImName,
+			     const std::string& OutputPath)
+{
+  size_t pos = OriginalImName.find_first_of(OriginalImName);
+
+  if(pos == std::string::npos)
+    {
+      UndistortedImName = OutputPath + "undistorted_" + OriginalImName;
+    }
+  else
+    {
+      UndistortedImName = OutputPath.substr(pos+1) + "undistorted_" + 
+	OriginalImName;
+    }
 }
 
 
@@ -107,8 +179,51 @@ void FillConfig(std::ofstream& ofm,
 		CvMat* rotation,
 		CvMat* translation)
 {
+  const int K_SIZE = 9;
+  const int R_SIZE = 9;
+  const int T_SIZE = 3;
+  
+  // Left image name
   ofm << UndistortedImNameL << '\t';
   
+  // K for left image
+  for(int i=0; i<K_SIZE; i++)
+    {
+      ofm << Intrinsic_L->data.fl[i] << '\t';
+    }
+  ofm << std::endl;
+
+  // R for left image, which is identity matrix
+  ofm << 1.0 << '\t' << 0.0 << '\t' << 0.0
+      << 0.0 << '\t' << 1.0 << '\t' << 0.0
+      << 0.0 << '\t' << 0.0 << '\t' << 1.0 << std::endl;
+
+  // T for left image, which is a 0 vector
+  ofm << 0.0 << '\t' << 0.0 << '\t' << std::endl;
+
+  // Right image name
+  ofm << UndistortedImNameR << '\t';
+
+  // K for right image
+  for(int i=0; i<K_SIZE; i++)
+    {
+      ofm << Intrinsic_R->data.fl[i] << '\t';
+    }
+  ofm << std::endl;
+
+  // R for right image
+  for(int i=0; i<R_SIZE; i++)
+    {
+      ofm << rotation->data.fl[i] << '\t';
+    }
+  ofm << std::endl;
+
+  // T for right image
+  for(int i=0; i<T_SIZE; i++)
+    {
+      ofm << translation->data.fl[i] << '\t';
+    }
+  ofm << std::endl;
 }
 
 
